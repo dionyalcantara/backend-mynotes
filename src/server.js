ee5bd344f231aaa30b7c93c3b0458fb6c56365fc
dionyguape <dionyguape@gmail.com>
2023-07-13 21:16:16 -0300

    Entendendo o Route Params

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const PORT = 3010;
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  res.send(`Id da rota: ${id}`);
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`));
