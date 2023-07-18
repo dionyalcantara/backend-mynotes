7ea0729bd88faaf030ea7ada2d03c90f70372b22
dionyguape <dionyguape@gmail.com>
2023-07-18 13:58:41 -0300

    Tratando Errors

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require('express-async-errors');
+const AppError = require('./utils/AppError')
+
 const express = require('express');
 const PORT = 3010;
 
@@ -7,6 +10,22 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+})
 
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`));
