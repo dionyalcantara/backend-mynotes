beba5b2691986f8b4eb2b77763bc2c9578d34a19
dionyguape <dionyguape@gmail.com>
2023-07-18 09:06:08 -0300

    Organizando as rotas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const PORT = 3010;
 
-const app = express();
-app.use(express.json())
+const routes = require('./routes')
 
-app.post('/users', (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({name, email, password});
-});
+const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`));
