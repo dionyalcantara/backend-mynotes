0e4c2be0df2c463eb6ed1a691ad2736866b35b08
dionyguape <dionyguape@gmail.com>
2023-07-18 22:11:07 -0300

    Adicionando o banco de dados com Sqlite

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const usersRouter = require('./users.routes')
 
 const routes = Router();
 
-routes.use('/users', usersRouter)
+routes.use('/users', usersRouter);
 
 module.exports = routes;
 
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 require('express-async-errors');
-const AppError = require('./utils/AppError')
+const database = require('./database/sqlite');
+const AppError = require('./utils/AppError');
 
 const express = require('express');
 const PORT = 3010;
 
-const routes = require('./routes')
+const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
 
-app.use((error, req, res, next) => {
+database();
+
+app.use((error, req, res, _next) => {
   if(error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: 'error',
