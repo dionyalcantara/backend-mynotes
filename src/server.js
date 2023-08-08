3ef9f82ad55d561ad21f8f7db0677a206d0776c1
dionyguape <dionyguape@gmail.com>
2023-08-08 08:37:51 -0300

    Automatizando a criação da tabela com migrations

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-const database = require('./database/sqlite');
+const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 
 const express = require('express');
@@ -7,12 +7,12 @@ const PORT = 3010;
 
 const routes = require('./routes');
 
+migrationsRun();
+
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-database();
 
 app.use((error, req, res, _next) => {
   if(error instanceof AppError) {
