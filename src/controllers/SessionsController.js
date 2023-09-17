66009536fc524bfa41d65d920457d03111142e0a
dionyguape <dionyguape@gmail.com>
2023-09-17 15:30:37 -0300

    Criando a rota de autenticação

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,8 @@
+class SessionsController {
+  async create(req, res){
+    const { email, password } = req.body;
+    return res.json({ email, password });
+  }
+}
+
+module.exports = SessionsController;
